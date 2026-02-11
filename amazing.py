31714aad8ce9ce2eacb5160ff3e8062156d60f1f
Cann0u <raphael.dedack@epitech.eu>
2026-02-11 15:34:13 +0100

    push

---
+++ amazing.py
@@ -0,0 +1,19 @@
+import random
+
+class Amaze():
+	def __init__(self, height: int, width: int):
+		self.witdh = witdh
+		self.height = height
+
+	def maze_generator(self) ->  list[list[int]]:
+		maze = [[1  for j in range(self.width * 2)] for i in range(self.height * 2)]
+		pos = [0, 0]
+		start = (0, 0)
+		def check_valid(maze: list[list[int]], x:int, y:int):
+			valid_way = []
+			if y - 2 > 0 and maze[y + 2][x] == 1
+
+		while True:
+
+		return maze
+print(maze_generator(4, 8))
