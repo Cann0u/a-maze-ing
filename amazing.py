0e828918c0b3ae822baba5c7b09b44346437067a
Cann0u <raphael.dedack@epitech.eu>
2026-02-17 13:49:19 +0100

    push amazing

--- amazing.py
+++ amazing.py
@@ -3,9 +3,20 @@ import curses as cs
 
 
 def main() -> None:
-    generator = MazeGenerator(20, 30)
+    generator = MazeGenerator(50, 50)
     maze = generator.maze_gen()
-    screen = cs.initscr()
-    for i in maze:
-        screen.addstr(i)
-        screen.refresh()
+    # screen = cs.initscr()
+    # for i in maze:
+    #     screen.addch("|")
+    #     for j in i:
+    #         if j == 0:
+    #             screen.addch("⬛")
+    #         else:
+    #             screen.addch("⬜")
+    #     screen.addch("|")
+    #     screen.addch("\n")
+    #     screen.refresh()
+    # screen.clear()
+
+
+main()
--- mazegen/__init__.py
+++ mazegen/__init__.py
@@ -1,5 +1,6 @@
 import random
-
+import curses as cs
+import time
 
 class MazeGenerator():
     def __init__(self, height: int, width: int):
@@ -10,26 +11,37 @@ class MazeGenerator():
         for i in range(2):
             x, y = pos
             h, w = direc
-            maze[x + h][y + w] = 0
+            maze[x + h][y + w] = "⬜"
             pos = (x + h, y + w)
         return pos
 
     def maze_gen(self) -> list[list[int]]:
-        maze = [[1 for j in range(self.width)] for i in range(self.height)]
+        height = self.height + self.height % 2 - 1
+        width = self.width + self.width % 2 - 1
+        maze = [["⬛" for j in range(width)] for i in range(height)]
         direc = [(-1, 0), (1, 0), (0, -1), (0, 1)]
         end = False
         prev = [(1, 1)]
         curr = prev.pop()
+        x, y = curr
+        maze[x][y] = "⬜"
+        screen = cs.initscr()
         while not end:
             valid_pos = []
             for i, j in direc:
-                if (i != 0 and curr[0] + i * 2 > 0
-                        and curr[0] + i * 2 < self.height
-                        and maze[curr[0] + i * 2][curr[1]] == 1):
+                if (
+                    i != 0
+                    and curr[0] + i * 2 > 0
+                    and curr[0] + i * 2 < height
+                    and maze[curr[0] + i * 2][curr[1]] == "⬛"
+                ):
                     valid_pos.append((i, j))
-                if (j != 0 and curr[1] + j * 2 > 0
-                        and curr[1] + j * 2 < self.width
-                        and maze[curr[0]][curr[1] + j * 2] == 1):
+                if (
+                    j != 0
+                    and curr[1] + j * 2 > 0
+                    and curr[1] + j * 2 < width
+                    and maze[curr[0]][curr[1] + j * 2] == "⬛"
+                ):
                     valid_pos.append((i, j))
             if len(valid_pos) == 0:
                 curr = prev.pop()
@@ -38,4 +50,9 @@ class MazeGenerator():
                 curr = self.break_wall(maze, curr, random.choice(valid_pos))
             if prev == []:
                 end = True
+            for i in maze:
+                screen.addstr(''.join(i) + '\n')
+            time.sleep(1/100)
+            screen.refresh()
+            screen.clear()
         return maze
